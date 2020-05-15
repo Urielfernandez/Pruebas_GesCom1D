b90c392242a72b2ca93711430175bfed3637df6e
Damian <damian.cruz@rai.usc.es>
2020-05-15 12:21:52 +0200

    TestValorarPlatos creado con métodos, pero sin implementar aún

---
+++ PrjGrupo1D-vFinalv7/test/ValorarPlatos/TestValorarPlatos.java
@@ -0,0 +1,119 @@
+/**
+ * 
+ */
+package ValorarPlatos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import GestionPagosYCalificaciones.GestionPagosYCalificaciones;
+
+/**
+ * @author Damián Cruz García
+ *
+ */
+class TestValorarPlatos {
+	private static GestionPagosYCalificaciones gestionCalificaciones;
+	
+	/**
+	 * @throws java.lang.Exception
+	 */
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		gestionCalificaciones=new GestionPagosYCalificaciones();
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/////
+	@Test
+	@DisplayName("P-02-01")
+	void testP02_C1() {
+		fail("Not yet implemented");
+		//Arrange
+		//Act
+		//Assert
+	}
+	
+	@Test
+	@DisplayName("P-02-02")
+	void testP02_C2() {
+		fail("Not yet implemented");
+		//Arrange
+		//Act
+		//Assert
+	}
+	
+	@Test
+	@DisplayName("P-02-03")
+	void testP02_C3() {
+		fail("Not yet implemented");
+		//Arrange
+		//Act
+		//Assert
+	}
+	
+	@Test
+	@DisplayName("P-02-04")
+	void testP02_C4() {
+		fail("Not yet implemented");
+		//Arrange
+		//Act
+		//Assert
+	}
+	
+	@Test
+	@DisplayName("P-02-05")
+	void testP02_C5() {
+		fail("Not yet implemented");
+		//Arrange
+		//Act
+		//Assert
+	}
+
+	@Test
+	@DisplayName("P-02-06")
+	void testP02_C6() {
+		fail("Not yet implemented");
+		//Arrange
+		//Act
+		//Assert
+	}
+
+	@Test
+	@DisplayName("P-02-07")
+	void testP02_C7() {
+		fail("Not yet implemented");
+		//Arrange
+		//Act
+		//Assert
+	}
+
+	@Test
+	@DisplayName("P-02-08")
+	void testP02_C8() {
+		fail("Not yet implemented");
+		//Arrange
+		//Act
+		//Assert
+	}
+
+}
