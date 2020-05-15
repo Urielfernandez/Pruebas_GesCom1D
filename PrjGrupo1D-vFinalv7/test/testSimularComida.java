aa1a458c67a0f06b1908aa0186b9a2063fee6dc9
Urielfernandez <urielfernandezgranha@gmail.com>
2020-05-15 09:21:26 +0200

    yeyouuuuu

---
+++ PrjGrupo1D-vFinalv7/test/testSimularComida.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testSimularComida {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
